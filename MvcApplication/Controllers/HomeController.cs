f1ca05ef698a38687ca5f227cf8a5618dc5547e1
Matteo Manzo <matteo.manzo@comparethemarket.com>
2015-07-19 17:55:59 +0100

    changed home controller to play with route config

--- MvcApplication/Controllers/HomeController.cs
+++ MvcApplication/Controllers/HomeController.cs
@@ -10,7 +10,13 @@ namespace MvcApplication.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var optional = RouteData.Values["id"];
+
+            var message = String.Format("{0}::{1} {2}", controller, action, optional);
+
+            ViewBag.Message = message;
 
             return View();
         }
